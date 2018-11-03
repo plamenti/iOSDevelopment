f1c8d7cd44523b9b140408fe9f4a563882f1cede
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-03 23:57:41 +0200

    Create IBOutlet for ImageView and IBAction for Ask button

--- Magic-8-Ball/Magic-8-Ball/ViewController.swift
+++ Magic-8-Ball/Magic-8-Ball/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func askButtonPressed(_ sender: Any) {
+    }
 }
 
