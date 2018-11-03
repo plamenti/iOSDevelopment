bc68ddb73c6af207bfe0e95476969d376c899c76
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-03 16:51:59 +0200

    Fix warning messages, add variables for random numbers

--- Dicee-iOS/Dicee/Dicee/ViewController.swift
+++ Dicee-iOS/Dicee/Dicee/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -18,8 +20,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
     @IBAction func rollButtonPresses(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex2)
     }
 }
 
