33252f76241e9e73f7d6d9845d11dee101e2c00d
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-03 18:21:00 +0200

    Implement motion detection functionality

--- Dicee-iOS/Dicee/Dicee/ViewController.swift
+++ Dicee-iOS/Dicee/Dicee/ViewController.swift
@@ -33,5 +33,9 @@ class ViewController: UIViewController {
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 }
 
