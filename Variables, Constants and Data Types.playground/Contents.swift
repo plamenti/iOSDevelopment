12e5fa392899640a2d508bc1c5d795e47d4cf05d
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-03 17:23:52 +0200

    Add examples for Variables, Constants and Data Types

--- Variables, Constants and Data Types.playground/Contents.swift
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var str = "Hello, playground"
+// variables and constants can have explicitly given type, but this can be omitted
+
+// variable
+var myAge : Int = 32
+
+myAge = 33
+
+// constant
+let myName : String = "Plamen"
+
+let myFamilyName : String = "Kos"
+
+// some kind like string interpolation
+let myFullName = "\(myName) \(myFamilyName)"
+
+// if variable of type Int is in string it is converted to string
+let myDetails = "\(myFullName), \(myAge)"
+
+let wholeNumbers : Int = 42
+let text : String = "abc"
+let booleans : Bool = true
+let floatingNumbers : Float = 3.141592
+let doubleNUmbers : Double = 3.141592653589793
+
