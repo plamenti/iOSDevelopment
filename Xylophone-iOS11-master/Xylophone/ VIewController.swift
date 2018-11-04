c2801ca64e97938bccebb575d9152123a80c2a9a
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-04 21:43:40 +0200

    Impement playing sound

--- Xylophone-iOS11-master/Xylophone/ VIewController.swift
+++ Xylophone-iOS11-master/Xylophone/ VIewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var audioPlayer : AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension:"wav")
         
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch{
+            print(error)
+        }
+        
+        audioPlayer.play()
     }
 
 }
