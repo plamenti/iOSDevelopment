200af8f4f9189fe9d260aed684aa51804db3d6c9
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-04 12:11:05 +0200

    Implement BMI calculator

--- BMI Calculator.playground/Contents.swift
+++ BMI Calculator.playground/Contents.swift
@@ -1,2 +1,17 @@
 import UIKit
 
+func calculateBodyMassindex(mass: Double, height: Double) -> String{
+    let bmi = mass / (height * height)
+    
+    if bmi > 25.0{
+        return "Overweight"
+    }
+    else if bmi >= 18.5 && bmi <= 25.0{
+        return "Normal"
+    }
+    else{
+        return "Underweight"
+    }
+}
+
+print(calculateBodyMassindex(mass: 82, height: 1.82))
