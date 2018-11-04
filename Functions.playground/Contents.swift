1856684b16a1c28ec364c23ade1188a6415f9c81
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-04 11:26:05 +0200

    Add create function example

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,2 +1,10 @@
-import UIKit
+// create a function
+func getMilk(){
+    print("go to the shops")
+    print("buy 2 cartons of milk")
+    print("pay")
+    print("come home")
+}
 
+//execute the function
+getMilk()
