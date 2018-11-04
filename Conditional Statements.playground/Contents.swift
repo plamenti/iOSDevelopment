2fb877fc103190aa9dddfee25ca559a7743a02a5
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-04 11:58:29 +0200

    Create if-else example

--- Conditional Statements.playground/Contents.swift
+++ Conditional Statements.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
+func loveCalculator(yourName : String, theirName : String) -> String {
+    let loveScore = Int.random(in: 0...100)
+    
+    if(loveScore > 80){
+        return "You love score is \(loveScore). You love each other!"
+    }
+    else if loveScore > 40 && loveScore <= 80{
+        return "You love score is \(loveScore). You are together like coke and mentos!"
+    }
+    else{
+        return "You love score is \(loveScore). No love possible!"
+    }
+}
 
+print(loveCalculator(yourName: "John", theirName: "Jane"))
