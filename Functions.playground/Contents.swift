2b64473a52995c171312ea769c6afa5a9b18b653
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-04 11:40:43 +0200

    Create function that return result

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -19,3 +19,19 @@ func getMilk(howManyMilkCartons : Int){
 }
 
 getMilk(howManyMilkCartons: 5)
+
+// create functions that return result
+func getMilkWithChange(howManyMilkCartons : Int, howMuchMoneyRobotHasGiven : Int) -> Int{
+    print("go to the shops")
+    print("buy \(howManyMilkCartons) cartons of milk")
+    let priceToPay = howManyMilkCartons * 2
+    print("pay $\(priceToPay)")
+    print("come home")
+    let change = howMuchMoneyRobotHasGiven - priceToPay
+    
+    return change
+}
+
+var amountOfChange = getMilkWithChange(howManyMilkCartons: 4, howMuchMoneyRobotHasGiven: 10)
+
+print("Hello master, here's your $\(amountOfChange) change")
