59b0f4a73a1b13bb831da4e04454d2d9791d5645
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-10 09:12:15 +0200

    Implement access to data in the controler

--- Quizzler-iOS11-master/Quizzler/ViewController.swift
+++ Quizzler-iOS11-master/Quizzler/ViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    // Access the DataBase
+    let allQuestions = QuestionBank()
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
+    // Starting point
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Get data from the DataBase
+        let firstQuestion = allQuestions.list[0]
+        
+        // Send get data to the View
+        questionLabel.text = firstQuestion.questinsText
     }
 
 
