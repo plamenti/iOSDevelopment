1fb8dc94cde60c554b9d771dea070d8d5a9fffc0
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-11 09:21:12 +0200

    Set intro screen, change status bar to light

--- Destini-iOS11-master/Destini/ViewController.swift
+++ Destini-iOS11-master/Destini/ViewController.swift
@@ -41,10 +41,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
     }
 
     
