d9ac82160f68237d5c0e3db1742371077e720a47
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-15 06:47:57 +0200

    Add Classes and Objects project

---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Plamen Kostadinov on 11/15/18.
+//  Copyright © 2018 plamenti. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
