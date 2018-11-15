7172df1b3125ff602778340fcd8e52ec85ce359f
Plamen Kostadinov <pkostadinov@SOF-MBP-021.local>
2018-11-15 06:58:20 +0200

    Create class Car

---
+++ Classes and Objects/Classes and Objects/Car.swift
@@ -0,0 +1,14 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by Plamen Kostadinov on 11/15/18.
+//  Copyright © 2018 plamenti. All rights reserved.
+//
+
+import Foundation
+
+class Car{
+    var color = "Black"
+    var numberOfSeats = 5
+}
