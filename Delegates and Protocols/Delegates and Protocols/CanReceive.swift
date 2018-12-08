eec58bf61815ff72e699157f4428cf55a408146a
plamenti <plamenti_@abv.bg>
2018-12-08 15:44:58 +0200

    Create protocol

---
+++ Delegates and Protocols/Delegates and Protocols/CanReceive.swift
@@ -0,0 +1,11 @@
+//
+//  Canreceive.swift
+//  Delegates and Protocols
+//
+//  Created by Plamen Kostadinov on 12/8/18.
+//  Copyright © 2018 plamenti. All rights reserved.
+//
+
+protocol CanReceive {
+    func dataReceived(data: String)
+}
