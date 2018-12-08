3211b7eaa45816b9534f8401bfc85a794cb6ae06
plamenti <plamenti_@abv.bg>
2018-12-08 15:26:27 +0200

    Prepare needed ViewControllers and elements

--- Delegates and Protocols/Delegates and Protocols/FirstViewController.swift
+++ Delegates and Protocols/Delegates and Protocols/FirstViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+    }
 }
 
---
+++ Delegates and Protocols/Delegates and Protocols/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Delegates and Protocols
+//
+//  Created by Plamen Kostadinov on 12/8/18.
+//  Copyright © 2018 plamenti. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendDataBackButton(_ sender: Any) {
+    }
+}
