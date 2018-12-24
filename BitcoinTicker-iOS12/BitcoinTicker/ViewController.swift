d6369281066935148487dae4559d51424f01e42e
plamenti <plamenti_@abv.bg>
2018-12-24 18:47:47 +0200

    Add and conform protocols UIPickerViewDataSource and UIPickerViewDelegate

--- BitcoinTicker-iOS12/BitcoinTicker/ViewController.swift
+++ BitcoinTicker-iOS12/BitcoinTicker/ViewController.swift
@@ -2,32 +2,35 @@
 //  ViewController.swift
 //  BitcoinTicker
 //
-//  Created by Angela Yu on 23/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["EUR","GBP", "JPY","MXN","USD","BGN"]
     var finalURL = ""
 
     //Pre-setup IBOutlets
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
 
     
     //TODO: Place your 3 UIPickerView delegate methods here
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
     
     
 
