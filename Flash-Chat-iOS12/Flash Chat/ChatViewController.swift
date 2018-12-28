06061a7bde30cb04fc2816e10d9b7c27899c7070
plamenti <plamenti_@abv.bg>
2018-12-28 16:35:42 +0200

    Implement message text field animation

--- Flash-Chat-iOS12/Flash Chat/ChatViewController.swift
+++ Flash-Chat-iOS12/Flash Chat/ChatViewController.swift
@@ -6,10 +6,10 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // Declare instance variables here
-
+    
     
     // We've pre-linked the IBOutlets
     @IBOutlet var heightConstraint: NSLayoutConstraint!
@@ -26,21 +26,19 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.delegate = self
         messageTableView.dataSource = self
         
-        
         //TODO: Set yourself as the delegate of the text field here:
-
-        
+        messageTextfield.delegate = self
         
         //TODO: Set the tapGesture here:
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
         
-        
-
         //TODO: Register your MessageCell.xib file here:
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
         configureTableView()
     }
-
+    
     ///////////////////////////////////////////
     
     //MARK: - TableView DataSource Methods
@@ -65,7 +63,9 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     //TODO: Declare tableViewTapped here:
-    
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
     
     
     //TODO: Declare configureTableView here:
@@ -80,16 +80,24 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     //MARK:- TextField Delegate Methods
     
     
-
+    
     
     //TODO: Declare textFieldDidBeginEditing here:
-    
-    
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5){
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
     
     //TODO: Declare textFieldDidEndEditing here:
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.3){
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
     
-
     
     ///////////////////////////////////////////
     
@@ -111,7 +119,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     //TODO: Create the retrieveMessages method here:
     
     
-
+    
     
     
     
@@ -120,6 +128,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         //TODO: Log out the user and send them back to WelcomeViewController
         do {
             try Auth.auth().signOut()
+            print("Log out successful!")
             navigationController?.popToRootViewController(animated: true)
         }
         catch {
