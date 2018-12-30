9d947c17af545512bbf325eafd017a71bf37d333
plamenti <plamenti_@abv.bg>
2018-12-30 22:45:57 +0200

    Implement placing object in 3D using touch

--- ARDicee/ARDicee/ViewController.swift
+++ ARDicee/ARDicee/ViewController.swift
@@ -51,17 +51,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Enable some light
         sceneView.autoenablesDefaultLighting = true
-        
-        //        // Create a new scene
-        //        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-        //
-        //        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-        //
-        //            diceNode.position = SCNVector3(x: 0, y: 0, z:-0.1)
-        //            sceneView.scene.rootNode.addChildNode(diceNode)
-        //        }
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -90,10 +79,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
             
-            if !results.isEmpty {
-                print("Touched the plane")
-            } else {
-                print("Touched somewhere else")
+            //            if !results.isEmpty {
+            //                print("Touched the plane")
+            //            } else {
+            //                print("Touched somewhere else")
+            //            }
+            
+            if let hitResult = results.first {
+                print(hitResult)
+                
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    
+                    diceNode.position = SCNVector3(
+                        x: hitResult.worldTransform.columns.3.x,
+                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        z: hitResult.worldTransform.columns.3.z
+                    )
+                    
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                }
             }
         }
     }
