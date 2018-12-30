a7df638b1b40501a9c3786b25d20fe8dbe0427b8
plamenti <plamenti_@abv.bg>
2018-12-30 17:32:02 +0200

    Add .due and .png files

--- ARDicee/ARDicee/ViewController.swift
+++ ARDicee/ARDicee/ViewController.swift
@@ -20,32 +20,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Create some geometry - e.g. Cube
-        //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-        
-        // Create some geometry - Sphere with radius 20 centimeters
-        let sphere = SCNSphere(radius: 0.2)
-        
-        // Create material for the cube
-        let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "art.scnassets/8k_venus_surface.jpg")
-        
-        // Assign this material to materials array. In the example the array contains only one object
-        //        cube.materials = [material]
-        sphere.materials = [material]
-        
-        // Create node - this is a point in 3D space
-        let node = SCNNode()
-        // Give position to the node. If z is negative it is away from us!
-        node.position = SCNVector3(x:0, y:0.1, z:-0.5)
-        // Assign to that node what object to display - created cude
-        //        node.geometry = cube
-        // Assign to that node what object to display - created sphere
-        node.geometry = sphere
-        
-        // Put the node to a Scene view. It is added as a child node to a root node - there could be added more child nodes
-        // In example with the ship (plain) - ship is root, shipMesh is a childNode. And shipMech has also childNode - emiter
-        sceneView.scene.rootNode.addChildNode(node)
+//                // Create some geometry - e.g. Cube
+//                //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        
+//                // Create some geometry - Sphere with radius 20 centimeters
+//                let sphere = SCNSphere(radius: 0.2)
+//        
+//                // Create material for the cube
+//                let material = SCNMaterial()
+//                material.diffuse.contents = UIImage(named: "art.scnassets/8k_venus_surface.jpg")
+//        
+//                // Assign this material to materials array. In the example the array contains only one object
+//                //        cube.materials = [material]
+//                sphere.materials = [material]
+//        
+//                // Create node - this is a point in 3D space
+//                let node = SCNNode()
+//                // Give position to the node. If z is negative it is away from us!
+//                node.position = SCNVector3(x:0, y:0.1, z:-0.5)
+//                // Assign to that node what object to display - created cude
+//                //        node.geometry = cube
+//                // Assign to that node what object to display - created sphere
+//                node.geometry = sphere
+//        
+//                // Put the node to a Scene view. It is added as a child node to a root node - there could be added more child nodes
+//                // In example with the ship (plain) - ship is root, shipMesh is a childNode. And shipMech has also childNode - emiter
+//                sceneView.scene.rootNode.addChildNode(node)
         
         // Enable some light
         sceneView.autoenablesDefaultLighting = true
