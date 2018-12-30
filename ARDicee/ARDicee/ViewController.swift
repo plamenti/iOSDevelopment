07ae508862ec15e0dddf1810b355f68bee93996d
plamenti <plamenti_@abv.bg>
2018-12-30 21:43:50 +0200

    Enable sceneView debug options

--- ARDicee/ARDicee/ViewController.swift
+++ ARDicee/ARDicee/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
