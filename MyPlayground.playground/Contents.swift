e8d67e6f1365819d6658a5dd8cbbe896deaef594
plamenti <plamenti_@abv.bg>
2019-01-01 11:01:26 +0200

    Convert function to Closure and use it as function argument

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -27,3 +27,28 @@ func multiply(n1: Int, n2:Int) -> Int {
 
 calculate(n1: 2, n2: 3, operation: multiply)
 
+// Closures
+// Closures are anonymous functions
+// To convert a function to closure:
+// 1. Remove 'func' keyword
+// 2. Remove the name of the function
+// 3. Move open curly bracket at the front of the line
+// 4. Put 'in' keyword after the return type of the function
+// 5. Use it as argument
+// Example with calculate function - add substraction functionality:
+
+func substract(n1: Int, n2: Int) -> Int {
+    return n1 - n2
+}
+// Sustract function as closure
+// { (number1: Int, number2: Int) -> Int in
+//    return n1 - n2
+//}
+
+// Use it in calculate function
+calculate(n1: 3, n2: 2, operation: {
+    (number1: Int, number2: Int) -> Int in
+    return number1 - number2
+})
+
+
