1953954828d21065866956f4bc25a9dd33c85b93
plamenti <plamenti_@abv.bg>
2019-01-01 13:23:16 +0200

    Add UIViewController for tecond view

---
+++ Navigation Controller/Navigation Controller/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  Navigation Controller
+//
+//  Created by Plamen Kostadinov on 1.01.19.
+//  Copyright © 2019 Plamen Kostadinov. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
