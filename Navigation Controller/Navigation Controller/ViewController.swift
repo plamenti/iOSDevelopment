3ade8b12c8af6aebbad229216378fb19cbad940f
plamenti <plamenti_@abv.bg>
2019-01-01 13:07:17 +0200

    Create project and add one move View controller

---
+++ Navigation Controller/Navigation Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation Controller
+//
+//  Created by Plamen Kostadinov on 1.01.19.
+//  Copyright © 2019 Plamen Kostadinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
