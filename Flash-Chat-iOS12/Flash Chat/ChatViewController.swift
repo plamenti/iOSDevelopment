5a6a299ac085410ba6b7598617572962eb768655
plamenti <plamenti_@abv.bg>
2019-01-06 21:34:25 +0200

    Improve UX - remove TableView separation lines

--- Flash-Chat-iOS12/Flash Chat/ChatViewController.swift
+++ Flash-Chat-iOS12/Flash Chat/ChatViewController.swift
@@ -38,6 +38,8 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         configureTableView()
         retrieveMessages()
+        
+        messageTableView.separatorStyle = .none
     }
     
     ///////////////////////////////////////////
