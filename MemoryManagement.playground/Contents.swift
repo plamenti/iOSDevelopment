d0295cce51e29838f881619a3e69692ddd28c2ab
plamenti <plamenti_@abv.bg>
2019-01-29 09:41:50 +0200

    Implement Strong reference cycle - memory leak

--- MemoryManagement.playground/Contents.swift
+++ MemoryManagement.playground/Contents.swift
@@ -3,6 +3,12 @@ import UIKit
 class User {
     var name: String
     
+    private(set) var phones: [Phone] = []
+    func add(phone: Phone) {
+        phones.append(phone)
+        phone.owner = self
+    }
+    
     init(name: String) {
         self.name = name
         print("User \(name) is initialized")
@@ -13,6 +19,22 @@ class User {
     }
 }
 
+class Phone {
+    let model: String
+    var owner: User?
+    
+    init(model: String) {
+        self.model = model
+        print("Phone \(model) is initialized")
+    }
+    
+    deinit {
+        print("Phone \(model) is baing deallocated")
+    }
+}
+
 do {
     let user1 = User(name: "John")
+    let iPhone = Phone(model: "iPhone 7")
+    user1.add(phone: iPhone)
 }
